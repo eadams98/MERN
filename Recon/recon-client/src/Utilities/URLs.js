8311a0b8a7d8e2b331d787684feb8f0c668ca056
eadams98 <eric051598@gmail.com>
2024-08-01 18:49:29 -0700

    Update URLs.js

--- Recon/recon-client/src/Utilities/URLs.js
+++ Recon/recon-client/src/Utilities/URLs.js
@@ -1,6 +1,6 @@
 export const BASE_URL = "https://www.datareconreports.com";
 //export const BASE_URL = "http://localhost:4000";
-export const LOCAL_REPORT_URL = "https://www.datareconreports.com:4001";//"http://localhost:4001"
+export const LOCAL_REPORT_URL = "https://www.datareconreports.com/reports";//"http://localhost:4001"
 //export const LOCAL_REPORT_URL = "http://localhost:4001"
 
 export const BASE_URL_AWS = "https://www.datareconreports.com"; //port 4000
